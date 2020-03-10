8b4027eadac806579054f080566d78affc42e514
Dj <ritchey0713@gmail.com>
2020-03-10 17:15:03 -0400

    add new change events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   password = ""
   includesLetters = false
+  includesNumbers = false
+  includesSymbols = false
 
   onButtonClick = () => {
     console.log(this.includesLetters)
+    console.log(this.includesNumbers)
+    console.log(this.includesSymbols)
     this.password = 'HELLO WORLD'
   }
 
   onChangeIncludeLetters = () => {
     this.includesLetters = !this.includesLetters
+  }
+
+  onChangeIncludeNumbers = () => {
+    this.includesNumbers = !this.includesNumbers
+  }
+
+  onChangeIncludeSymbols = () => {
+    this.includesSymbols = !this.includesSymbols
   }
 }
