53ce3d9efd76ffd33d47c46579b8e00d07bb8fcb
Dj <ritchey0713@gmail.com>
2020-03-10 20:16:26 -0400

    add basic password creator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  password = ""
-  includesLetters = false
-  includesNumbers = false
-  includesSymbols = false
+  password = '';
+  includesLetters = false;
+  includesNumbers = false;
+  includesSymbols = false;
+  length = 0;
+
+  onChangeLength = (value: string) => {
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
 
   onButtonClick = () => {
-    console.log(this.includesLetters)
-    console.log(this.includesNumbers)
-    console.log(this.includesSymbols)
-    this.password = 'HELLO WORLD'
+    console.log("test")
+    const numbers = "1234567890"
+    const letters = "abcdefghijklmnopqrstuvwxyz"
+    const symbols = "!@#$%^&*()"
+
+    let validchars = ''
+    if(this.includesLetters){
+      validchars += letters
+    }
+
+    if(this.includesNumbers){
+      validchars += numbers
+    }
+
+    if(this.includesSymbols){
+      validchars += symbols
+    }
+
+    let generatedPassword = ""
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validchars.length)
+      generatedPassword += validchars[index]
+    }
+    this.password = generatedPassword;
+    console.log(generatedPassword)
   }
 
   onChangeIncludeLetters = () => {
-    this.includesLetters = !this.includesLetters
+    this.includesLetters = !this.includesLetters;
   }
 
   onChangeIncludeNumbers = () => {
-    this.includesNumbers = !this.includesNumbers
+    this.includesNumbers = !this.includesNumbers;
   }
 
   onChangeIncludeSymbols = () => {
-    this.includesSymbols = !this.includesSymbols
+    this.includesSymbols = !this.includesSymbols;
   }
 }
